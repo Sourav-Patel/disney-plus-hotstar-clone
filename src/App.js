4a17032356029263572927974d892b50a10e4f92
Sourav-Pate1 <sourav_patel@persistent.com>
2024-12-09 14:36:46 +0530

    Added Detail page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from './components/Detail';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-     </div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
